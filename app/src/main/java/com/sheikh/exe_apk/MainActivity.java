6a1f3bfa8932236e4b58c66cef0ef528b82db603
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-09-14 09:37:01 +0000

    add enpty project

---
+++ app/src/main/java/com/sheikh/exe_apk/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sheikh.exe_apk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        dotEnv dotEnv = new dotEnv();
+        textView.setText(dotEnv.OUR_TEXT);
+    }
+}
---
+++ app/src/main/java/com/sheikh/exe_apk/dotEnv.java
@@ -0,0 +1,5 @@
+package com.sheikh.exe_apk;
+
+public class dotEnv {
+    public final String OUR_TEXT = "hello Sheikh El-Moctar";
+}
