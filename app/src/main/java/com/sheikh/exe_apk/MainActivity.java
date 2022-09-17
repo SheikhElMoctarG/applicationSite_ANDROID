2e0ef28dd5c21daac7590022332c2605776d04f6
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-09-17 12:46:06 +0000

    add splash screen code

--- app/src/main/java/com/sheikh/exe_apk/MainActivity.java
+++ app/src/main/java/com/sheikh/exe_apk/MainActivity.java
@@ -2,18 +2,38 @@ package com.sheikh.exe_apk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textView);
-        dotEnv dotEnv = new dotEnv();
-        textView.setText(dotEnv.OUR_TEXT);
+        getSupportActionBar().hide();
+        logo = findViewById(R.id.logo);
+        name = findViewById(R.id.text_logo);
+        logo.setImageResource(R.drawable.logo_e);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, 10000);
+    }
+
+    public static void getPosts(){
+
     }
 }
