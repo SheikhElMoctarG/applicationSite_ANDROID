a335c397dd7df7d4f83cc17d1e28c66b02acb182
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-09-19 12:00:27 +0000

    fix it the connection to server

--- app/src/main/java/com/sheikh/exe_apk/HomeActivity.java
+++ app/src/main/java/com/sheikh/exe_apk/HomeActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HomeActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
 
+public class HomeActivity extends AppCompatActivity {
+    JSONArray posts = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Bundle extras = getIntent().getExtras();
-        if (extras != null)
-            Log.d("dataVolley", extras.getString("posts"));
+        if (extras != null) {
+            try {
+                posts = new JSONArray(extras.getString("posts"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
